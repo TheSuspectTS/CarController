33676e8dc2077cdbb1c949dfdc904d716ed91f36
SnowDice123 <110529277+SnowDice123@users.noreply.github.com>
2022-08-03 13:12:43 +0300

    bebra

---
+++ Assets/Scripts/menu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class menu : MonoBehaviour
+{
+
+
+    public void Playgame()
+    {
+        SceneManager.LoadScene("Demo");
+        Cursor.visible = false;
+    }
+    public void Playmultipalyer()
+    {
+        SceneManager.LoadScene("Loading");
+        Cursor.visible = true;
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Chustom()
+    {
+        SceneManager.LoadScene("Chustom");
+;    }
+
+    public void Choise()
+    {
+        SceneManager.LoadScene("host");
+    }
+
+
+}

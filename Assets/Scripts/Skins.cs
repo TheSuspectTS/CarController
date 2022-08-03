cbe6111cf9ae5515b8751d661df72ef50ad7fc43
SnowDice123 <110529277+SnowDice123@users.noreply.github.com>
2022-08-03 17:10:33 +0300

    sssssss

--- Assets/Scripts/Skins.cs
+++ Assets/Scripts/Skins.cs
@@ -49,6 +49,11 @@ public class Skins : MonoBehaviour
         PlayerPrefsX.SetBoolArray("StockArray", StockCheck);
     }
 
+    public void Spawn()
+    {
+
+    }
+
     public void ScrollRight()
     {
         if (index < player.childCount)
